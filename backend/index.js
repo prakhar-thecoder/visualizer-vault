3e6bd0c399319d8f72845587a65dcf9d790edcf3
root-ranger <root.ranger@proton.me>
2025-06-22 15:53:33 +0530

    Updated visualizer form and visualiers page to show visualizers grouped by subject

--- backend/index.js
+++ backend/index.js
@@ -32,7 +32,7 @@ app.use((err, req, res, next) => {
 });
 
 app.get('/', (req, res) => {
-    res.status(200).json({ message: 'Backend is up and running!' });
+    res.redirect('/admin');
 });
 
 // Start server

ad4417ada09970d8faedc1898eac9699b1394437
root-ranger <root.ranger@proton.me>
2025-06-27 10:49:21 +0530

    Fixed warnings

--- frontend/src/pages/VisualizerDetails.js
+++ frontend/src/pages/VisualizerDetails.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useParams, useNavigate, Link } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import { FaArrowLeft, FaExternalLinkAlt, FaTags } from 'react-icons/fa';
 import { fetchVisualizer } from '../utils/fetchVisualizer';
 import Loader from '../components/Loader';
